0a35e0e7cba672b0417e4e5e139871782c528301
TuncayHp2 <tunc270@gmail.com>
2026-01-20 01:35:33 +0300

    log yapısı

---
+++ FiMetas/FiLogTypes.cs
@@ -0,0 +1,51 @@
+﻿namespace OrakUtilDotNetCore.FiMetas
+{
+  public static class FiLogTypes
+  {
+    public static string Info()
+    {
+      return "info";
+    }
+
+    public static string Notice()
+    {
+      return "notice";
+    }
+
+    public static string Error()
+    {
+      return "error";
+    }
+
+    public static string Alert()
+    {
+      return "alert";
+    }
+
+    public static string Critical()
+    {
+      return "critical";
+    }
+
+    public static string Warning()
+    {
+      return "warning";
+    }
+
+    public static string Debug()
+    {
+      return "debug";
+    }
+  }
+
+}
+/*
+    const EMERGENCY = 'emergency';
+    const ALERT     = 'alert';
+    const CRITICAL  = 'critical';
+    const ERROR     = 'error';
+    const WARNING   = 'warning';
+    const NOTICE    = 'notice';
+    const INFO      = 'info';
+    const DEBUG     = 'debug';
+*/
