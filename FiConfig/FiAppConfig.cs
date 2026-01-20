db4174c54b2b7dd8f65a02d6fda428f5fc6cf405
TuncayHp2 <tunc270@gmail.com>
2026-01-20 14:55:50 +0300

    up

--- FiConfig/FiAppConfig.cs
+++ FiConfig/FiAppConfig.cs
@@ -1,50 +1,47 @@
-﻿namespace OrakUtilDotNetCore.FiConfig
+﻿namespace OrakUtilDotNetCore.FiConfig;
+
+using System;
+using System.Configuration;
+using System.Drawing;
+
+public static class FiAppConfig
 {
-  using System;
-  using System.Configuration;
-  using System.Drawing;
+  public static bool boTestMode = false;
+  public static IFiConfigManager? fiConfig;
+  public static IFiLogManager? fiLog;
 
-
-  public static class FiAppConfig
+  public static void ConvertTestModeTrue()
   {
-    public static bool boTestMode = false;
-    public static IFiConfigManager? fiConfig;
-    public static IFiLogManager? fiLog;
-
-    public static void ConvertTestModeTrue()
-    {
-      boTestMode = true;
-    }
+    boTestMode = true;
+  }
 
 
-    public static string GetConnStringWthTest(string txProfile)
-    {
-      // config dosyasından key'den sonra test ile geleni alması için.
-      if (boTestMode == true) txProfile = txProfile + "-test";
-      return fiConfig?.GetConnString(txProfile);
-    }
+  public static string GetConnStringWthTest(string txProfile)
+  {
+    // config dosyasından key'den sonra test ile geleni alması için.
+    if (boTestMode == true) txProfile = txProfile + "-test";
+    return fiConfig?.GetConnString(txProfile);
+  }
 
-    public static string? GetConnString(string txProfile)
-    {
-      // config dosyasından key'den sonra test ile geleni alması için. ???review
-      if (boTestMode == true) txProfile = txProfile + "-test";
+  public static string? GetConnString(string txProfile)
+  {
+    // config dosyasından key'den sonra test ile geleni alması için. ???review
+    if (boTestMode == true) txProfile = txProfile + "-test";
 
-      return fiConfig?.GetConnString(txProfile);
-    }
+    return fiConfig?.GetConnString(txProfile);
+  }
 
-    public static string? GetBaseUrl(string txProfile)
-    {
-      // config dosyasından key'den sonra test ile geleni alması için.
-      //if (boTestMode == true) txProfile = txProfile + "-test";
+  public static string? GetBaseUrl(string txProfile)
+  {
+    // config dosyasından key'den sonra test ile geleni alması için.
+    //if (boTestMode == true) txProfile = txProfile + "-test";
 
-      return fiConfig?.GetApiUrl(txProfile);
-    }
+    return fiConfig?.GetApiUrl(txProfile);
+  }
 
-    public static void LogMessage(string message)
-    {
-      fiLog?.Debug(message);
-    }
-
+  public static void LogDebug(string message)
+  {
+    fiLog?.Debug(message);
   }
 
 }
