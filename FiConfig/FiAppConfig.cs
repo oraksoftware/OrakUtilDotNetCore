24a8c9713e423bf62765f781551aba10de10df0a
TuncayHp2 <tunc270@gmail.com>
2026-08-24 08:50:14 +0300

    ups and rfc

--- FiConfig/FiAppConfig.cs
+++ FiConfig/FiAppConfig.cs
@@ -6,17 +6,23 @@ using System.Drawing;
 
 public static class FiAppConfig
 {
-  public static bool boTestMode = false;
-  public static IFiConfig? fiConfig;
-  public static IFiLogger? fiLog;
+  public static bool boTestMode; // { get; set; }
+  public static IFiConfig? fiConfig; // { get; set; }
+  public static IFiLogger? fiLog; // { get; set; }
 
   public static void ConvertTestModeTrue()
   {
     boTestMode = true;
   }
 
+  public static bool getBoTestModeNtn()
+  {
+    if (boTestMode==null) return false;
+    return boTestMode;
+  }
 
-  public static string GetConnStringWthTest(string txProfile)
+
+  public static string GetConnStringWitTest(string txProfile)
   {
     // config dosyasından key'den sonra test ile geleni alması için.
     if (boTestMode == true) txProfile = txProfile + "-test";
